86c12297e223d426897d0e8c609e78b622ba84fb
Grey Chen <957024640@qq.com>
2018-07-02 19:30:57 -0700

    fix: add login info
    fix: add login info

--- deploy.py
+++ deploy.py
@@ -1,14 +1,15 @@
 import subprocess
 from os import system
 
-version = subprocess.check_output(['git', 'describe', '--tags']).decode("utf-8").strip()[1:6]
+version = subprocess.check_output(['git', 'describe', '--tags']).decode("utf-8").strip()[1:]
 repo_name = "test-cd:{}".format(version)
 print("=======================Current latest release is {}".format(version))
 system("docker build -t {} .".format(repo_name))
 print("=======================Finished docking building")
 system("docker tag {} 856077326206.dkr.ecr.us-east-2.amazonaws.com/{}".format(repo_name, repo_name))
 print("=======================Finished docker tagging")
-system("docker login -u AWS -p $AWS_TOKEN https://856077326206.dkr.ecr.us-east-2.amazonaws.com")
+login_coommand = subprocess.check_output(['aws' ,'ecr' ,'get-login' ,'--no-include-email' ,'--region' ,'us-east-2']).decode("utf-8").strip()
+system(login_coommand)
 print("=======================Finished docker login")
 system("docker push 856077326206.dkr.ecr.us-east-2.amazonaws.com/{}".format(repo_name))
 print("=======================Finished publishing")
