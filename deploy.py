8f4077a5e4bdab6b8bca24b219938003b003e121
Grey Chen <957024640@qq.com>
2018-07-02 18:55:33 -0700

    fix: fix deploy.py
    fix: fix deploy.py

--- deploy.py
+++ deploy.py
@@ -4,7 +4,7 @@ from os import system
 version = subprocess.check_output(['git', 'describe', '--tags']).decode("utf-8").strip()[1:6]
 repo_name = "test-cd:{}".format(version)
 print("=======================Current latest release is {}".format(version))
-system("docker build -t test-cd .")
+system("docker build -t {} .".format(repo_name))
 print("=======================Finished docking building")
 system("docker tag {} 856077326206.dkr.ecr.us-east-2.amazonaws.com/{}".format(repo_name, repo_name))
 print("=======================Finished docker tagging")
