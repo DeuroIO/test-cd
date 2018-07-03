9a9efd93c16857a0ccdf4629c22fe2eef02b33d3
Grey Chen <957024640@qq.com>
2018-07-02 18:47:05 -0700

    BREAKING CHANGE: add Dockerfile and deploy.py

---
+++ deploy.py
@@ -0,0 +1,12 @@
+import subprocess
+from os import system
+
+version = subprocess.check_output(['git', 'describe', '--tags']).decode("utf-8").strip()[1:]
+repo_name = "test-cd:{}".format(version)
+print("=======================Current latest release is {}".format(version))
+system("docker build -t test-cd .")
+print("=======================Finished docking building")
+system("docker tag {} 856077326206.dkr.ecr.us-east-2.amazonaws.com/{}".format(repo_name, repo_name))
+print("=======================Finished docker tagging")
+system("docker push 856077326206.dkr.ecr.us-east-2.amazonaws.com/{}".format(repo_name))
+print("=======================Finished publishing")
